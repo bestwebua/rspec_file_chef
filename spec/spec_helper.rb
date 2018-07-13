e466f11c9964b169662bfafacc5a340e3e20d244
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-07-13 09:27:57 +0300

    Add autoload rspec customizations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 require 'bundler/setup'
 require 'rspec_file_env'
 
+rspec_custom = File.join(File.dirname(__FILE__), 'support/**/*.rb')
+Dir[File.expand_path(rspec_custom)].each { |file| require file }
+
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
   config.disable_monkey_patching!
   config.expect_with :rspec do |c|
     c.syntax = :expect
