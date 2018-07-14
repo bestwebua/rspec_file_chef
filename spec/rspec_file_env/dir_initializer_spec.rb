62aeef32c5b6f71cc191600be0953e1c76a66422
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-07-14 12:11:10 +0300

    Add #rspec_path, #set_rspec_path tests

--- spec/rspec_file_env/dir_initializer_spec.rb
+++ spec/rspec_file_env/dir_initializer_spec.rb
@@ -2,16 +2,16 @@ module RspecFileEnv
   TestClass = Class.new do
     include DirInitializer
     extend Dry::Configurable
-
     setting :rspec_path
     setting :custom_tmp_dir
     setting :custom_test_dir
   end
 
   RSpec.describe TestClass do
-    let(:rspec_path) { File.expand_path(__dir__) }
-    let(:custom_tmp_dir) { "#{rspec_path}/tmp_dir" }
-    let(:custom_test_dir) { "#{rspec_path}/test_dir" }
+    let(:this_path)       { File.expand_path(__dir__) }
+    let(:rspec_path)      { this_path }
+    let(:custom_tmp_dir)  { "#{this_path}/tmp_dir" }
+    let(:custom_test_dir) { "#{this_path}/test_dir" }
     let(:apply_config) do
       subject.class.configure do |config|
         config.rspec_path = rspec_path
@@ -41,7 +41,7 @@ module RspecFileEnv
 
     describe 'class config getters' do
       before { apply_config }
-      
+
       context '#default_rspec_path' do
         it "returns #{TestClass}.config.rspec_path" do
           expect(subject.class.config.rspec_path).to eq(rspec_path)
@@ -57,6 +57,55 @@ module RspecFileEnv
       context '#custom_test_dir' do
         it "returns #{TestClass}.config.custom_test_dir" do
           expect(subject.class.config.custom_test_dir).to eq(custom_test_dir)
+        end
+      end
+    end
+
+    describe '#rspec_path' do
+      specify { expect(subject.rspec_path).to be_nil }
+    end
+
+    describe '#set_rspec_path' do
+      describe 'scenario' do
+        before { apply_config }
+
+        describe 'not raise error' do
+          before { subject.send(:set_rspec_path) }
+
+          context 'rspec path not set' do
+            before do
+              allow(subject).to receive(:default_rspec_path)
+              subject.send(:set_rspec_path)
+            end
+
+            after { subject.send(:set_rspec_path) }
+            
+            let(:rspec_path) { nil }
+
+            specify { expect(subject).to receive(:default_rspec_path) }
+            specify { expect(subject.rspec_path).to be_nil }
+          end
+
+          context 'right rspec path' do
+            before do
+              allow(subject).to receive(:rspec_pattern)
+              subject.send(:set_rspec_path)
+            end
+
+            after { subject.send(:set_rspec_path) }
+
+            specify { expect(subject).to receive(:rspec_pattern) }
+            specify { expect(subject.rspec_path).to_not be_nil }
+          end
+        end
+
+        describe 'raise error' do
+          context 'wrong rspec path' do
+            let(:rspec_path) { '/some_wrong_path/without/spec_folder' }
+            specify do
+              expect {subject.send(:set_rspec_path)}.to raise_error(RuntimeError, Error::RSPEC_PATH)
+            end
+          end
         end
       end
     end
