ad40ce75d51d5a5ba91a285f7c9df587ff9abf8f
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-07-15 14:06:06 +0300

    Add #initialize to TestClass helper

--- spec/support/helpers/test_class.rb
+++ spec/support/helpers/test_class.rb
@@ -3,8 +3,13 @@ module RspecFileChef
     include DirInitializer
     include StateKeeper
     extend Dry::Configurable
+    
     setting :rspec_path
     setting :custom_tmp_dir
     setting :custom_test_dir
+
+    def initialize
+      @tracking_files, @path_table = [], {}
+    end
   end
 end
